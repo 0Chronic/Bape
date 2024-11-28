04c42be0c0d9ce705b1b5c53ac496ae09b60e4d7
0Chronic <kyawn1063@gmail.com>
2024-11-29 02:03:08 +0630

    nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+"use client"
+
+import React,{useState} from 'react'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from '../public/assets/logo.png'
-import {AiOutlineMenu} from 'react-icons/ai'
+import {AiOutlineMenu,AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
+
+
+  const [nav, setNav ] = useState (false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
+
   return (
     <div>
       <div className='flex items-center justify-between w-full h-full px-2 2xl:px-16'>
@@ -41,16 +53,66 @@ const Navbar = () => {
 
             {/* Hamburger Icon */}
             <div
+
+            onClick={handleNav}
             
             className='md:hidden'
           >
             <AiOutlineMenu size={25}/>
           </div>
-         
 
-        </div>
+          {/*mobile view */}
+
+            <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70': ''} >
+              <div className={nav? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' : 'fixed left-[-100%] top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'}>
+               
+
+               <div>
+              <div className='flex w-full items-center justify-between'>
+                <Image src={logo}  width={70} height={30} />
+                <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                  <AiOutlineClose />
+                </div>
+              </div>
+               </div>
 
 
+               <div className='py-5 flex flex-col'>
+               <ul className=' md:flex'>
+            <li className='py-4 text-md uppercase hover:border-b'>
+              <Link href='/'>Home</Link>
+            </li>
+            <li className='py-4 text-md uppercase hover:border-b'>
+              <Link href='/#about'>About</Link>
+            </li>
+            <li className='py-4 text-md uppercase hover:border-b'>
+              <Link href='/#skills'>Skills</Link>
+            </li>
+            <li className='py-4 text-md uppercase hover:border-b'>
+              <Link href='/#projects'>Projects</Link>
+            </li>
+            <li className='py-4 text-md uppercase hover:border-b'>
+              <Link href='/resume'>Resume</Link>
+            </li>
+            <li className='py-4 text-md uppercase hover:border-b'>
+              <Link href='/#contact'>Contact</Link>
+            </li>
+          </ul>
+
+
+        
+               </div>
+
+             
+           
+
+       
+
+              </div>
+            </div>
+
+
+      </div>
       </div>
     
     </div>
