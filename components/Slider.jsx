14c7c45f04cf66770d78d4ef50f5bdd70329e30c
0Chronic <kyawn1063@gmail.com>
2024-11-30 02:10:25 +0630

    slidingg

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -19,20 +19,30 @@ const Slider = () => {
     const [currentIndex, setCurrentIndex] = useState(0)
 
     const prevSlide = () => {
+     const isFirstSlide = currentIndex === 0;
+     const newIndex = isFirstSlide ? slides.length -1 : currentIndex -1;
+     setCurrentIndex(newIndex)
       
+    }
+    const nextSlide = () => {
+      const isLastSlide = currentIndex === slides.length -1;
+      const newIndex = isLastSlide ? 0 : currentIndex +1;
+      setCurrentIndex(newIndex)
+
+
     }
   return (
     <div className='max-w-[1900px] h-[1000px] w-full m-auto py-16 px-4 relative group'>
         <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'></div>
         {/*Left Arrow*/}
         <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-7 text-4xl rounded-full bg-black/40 text-white cursor-pointer'>
-          <BsChevronCompactLeft size={40} />
+          <BsChevronCompactLeft onClick={prevSlide} size={40} />
         </div>
 
           {/*Right Arrow*/}
 
           <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 right-7 transltae-y-[-50%] text-4xl rounded-full bg-black/40 text-white cursor-pointer'>
-            <BsChevronCompactRight size={40}/>
+            <BsChevronCompactRight onClick={nextSlide} size={40}/>
           </div>
     </div>
   )
