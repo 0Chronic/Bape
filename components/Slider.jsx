05ed8298b54009f80ca9ae7ed6c4ca8e93d99f85
0Chronic <kyawn1063@gmail.com>
2024-11-29 21:47:45 +0630

    sliding

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+"use client";
+
+import React,{useState} from  'react'
+import {BsChevronCompactLeft , BsChevronCompactRight} from 'react-icons/bs'
 
 const Slider = () => {
     const slides = [
         {
-            url: 'https://eu.bape.com/collections/bape-sta'
+            url: 'https://eu.bape.com/cdn/shop/files/New_Project_5.png?v=1730126876&width=1600'
         },
         {
           url: 'https://eu.bape.com/cdn/shop/files/2411_BAPE-X-ADIDAS_BANNER__2160x1080_9c000024-5ffb-4d89-b6d6-d4e5581ae030.jpg?v=1732528716&width=1600'
+        },
+        {
+          url: 'https://eu.bape.com/cdn/shop/files/Bape_STA_ICON_M2_banner_2160x1080_db90c01e-5e86-4ec6-bc75-0753af6b57ec.jpg?v=1732881270&width=1600'
         }
     ];
+
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    const prevSlide = () => {
+      
+    }
   return (
-    <div className='max-w-[1900px] h-[1000px] w-full m-auto py-16 px-4 relative'>
-        <div style={{backgroundImage: `url(${slides[1].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'></div>
+    <div className='max-w-[1900px] h-[1000px] w-full m-auto py-16 px-4 relative group'>
+        <div style={{backgroundImage: `url(${slides[currentIndex].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'></div>
+        {/*Left Arrow*/}
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-7 text-4xl rounded-full bg-black/40 text-white cursor-pointer'>
+          <BsChevronCompactLeft size={40} />
+        </div>
+
+          {/*Right Arrow*/}
+
+          <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 right-7 transltae-y-[-50%] text-4xl rounded-full bg-black/40 text-white cursor-pointer'>
+            <BsChevronCompactRight size={40}/>
+          </div>
     </div>
   )
 }
