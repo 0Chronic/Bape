38ad3b71c665b795dd46e2ea3949de516883ea16
0Chronic <kyawn1063@gmail.com>
2024-12-02 01:18:21 +0630

    error

--- app/mockData.js
+++ app/mockData.js
@@ -14,5 +14,29 @@ export const data = [
     {
         id: 4,
         img: 'https://eu.bape.com/cdn/shop/files/00ABAK701305_BRW_B.jpg?v=1732293815&width=700'
+    },
+    {
+        id: 5,
+        img: 'https://eu.bape.com/cdn/shop/files/001LTK301302_BLK_B.jpg?v=1732293571&width=700'
+    },
+    {
+        id: 6,
+        img: 'https://eu.bape.com/cdn/shop/files/001LJK801319_IVY_B.jpg?v=1732293407&width=700'
+    },
+    {
+        id: 7,
+        img: 'https://eu.bape.com/cdn/shop/files/0MXJWW4340XXNSVZ-02.jpg?v=1732293259&width=700'
+    },
+    {
+        id: 8,
+        img: 'https://eu.bape.com/cdn/shop/files/001FWK801303M_BLK_B.jpg?v=1732288409&width=700'
+    },
+    {
+        id: 9,
+        img: 'https://eu.bape.com/cdn/shop/files/001DNK802001_GRA_B.jpg?v=1732336278&width=700'
+    },
+    {
+        id: 10,
+        img: 'https://eu.bape.com/cdn/shop/files/001CSK701007_BLK_A.jpg?v=1732182109&width=1200'
     }
 ]
--- components/Horizon.jsx
+++ components/Horizon.jsx
@@ -1,16 +1,23 @@
+"use client";
 import React from 'react'
 import { data } from '@/app/mockData'
+import {MdChevronLeft , MdChevronRight} from 'react-icons/md'
+import { document, fromJSON } from 'postcss'
 
 const Horizon = () => {
+
+ 
   return (
     <>
         <h1></h1>
         <div className='ralative flex items-center'>
-       <div id='slider' className='w-full h-full overflow-x-scroll scroll'>
+            <MdChevronLeft   size={40} />
+       <div id='slider' className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth'>
        {data.map((item)=>(
             <img className='w-[400px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300' src={item.img} alt='/' />
          ))}
        </div>
+       <MdChevronRight  size={40} />
         </div>
     </>
   )
